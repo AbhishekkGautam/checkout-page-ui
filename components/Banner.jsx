151da5fa929a096b8ea43175bb50984f5c4bc105
Abhishek Gautam <abhishekgautam243@gmail.com>
2024-02-08 23:39:48 +0530

    add banner

---
+++ components/Banner.jsx
@@ -0,0 +1,11 @@
+const Banner = () => {
+  return (
+    <div className="h-10 bg-black flex items-center justify-center">
+      <p className="text-sm font-semibold text-grey-100">
+        Holding your tickets for 30:00
+      </p>
+    </div>
+  );
+};
+
+export default Banner;
