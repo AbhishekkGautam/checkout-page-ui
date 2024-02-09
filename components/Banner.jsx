a57892ffb2961c71b92558a967360b5ab5370d96
Abhishek Gautam <abhishekgautam243@gmail.com>
2024-02-09 23:10:05 +0530

    style banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,7 +1,7 @@
 const Banner = () => {
   return (
     <div className="h-10 bg-black flex items-center justify-center">
-      <p className="text-sm font-semibold text-grey-100">
+      <p className="text-xs lg:text-sm font-semibold text-grey-100">
         Holding your tickets for 30:00
       </p>
     </div>
