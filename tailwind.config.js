95364b180b5b0fb728286aae0c3518d93c13fb10
Abhishek Gautam <abhishekgautam243@gmail.com>
2024-02-09 23:07:35 +0530

    add more colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,25 @@ module.exports = {
         greySecondary: '#60646C',
         greyTertiary: '#7E808A',
         lightGrey: 'rgba(249, 249, 251, 0.25)',
+        greyDisabled: '#8B8D98',
+        greyDivider: '#3C3F44',
         grey: {
           100: '#FCFCFD',
           200: '#D3D4DB',
           300: '#B9BBC6',
           400: '#DDDDE3',
+          500: '#EDEEF0',
+          600: '#F9F9FB',
+          700: '#E4E4E9',
+          800: '#ADB1B8',
+          900: '#7D828A',
+        },
+        green: {
+          primary: '#299764',
+          secondary: '#E9F9EE',
+        },
+        indigo: {
+          primary: '#3A5CCC',
         },
       },
       fontFamily: {
