540ff6d4fbdc872dbd8e993847d7386bf39f9c3c
Abhishek Gautam <abhishekgautam243@gmail.com>
2024-02-10 09:05:10 +0530

    add form input component

---
+++ components/form/FormInput.jsx
@@ -0,0 +1,16 @@
+const FormInput = ({ type, placeholder, ...props }) => {
+  return (
+    <div className="relative">
+      <input
+        type={type}
+        className="w-full p-4 border border-grey-300 rounded-xl lg:rounded-2xl z-10 bg-transparent relative"
+        {...props}
+      />
+      <label className="absolute z-1 inset-y-0 start-0 flex items-center ps-4 text-greyTertiary">
+        {placeholder} <span className="text-red-500 ml-1">*</span>
+      </label>
+    </div>
+  );
+};
+
+export default FormInput;
